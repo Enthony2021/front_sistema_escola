39b2acd83afece38ff5d4d0fd8f162cd4eec4ff7
Enthony Stevie <enthonyoliveira96@gmail.com>
2023-08-15 14:19:05 -0700

    Ainda densenvolvendo

--- src/pages/Alunos/index.js
+++ src/pages/Alunos/index.js
@@ -1,14 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+// Para buscar um valor dentro um objeto ou setar um valor padrão se não encontrar
+import { get } from 'lodash';
+
+import { FaUserCircle } from 'react-icons/fa';
 
 import AlunosStyled from './styled';
 import { Container } from '../../styles/GlobalStyles';
+import axios from '../../services/axios';
 
 const Alunos = () => {
+  const [alunos, setAlunos] = useState([]);
+
+  useEffect(() => {
+    async function getData() {
+      const response = await axios.get('/alunos');
+      setAlunos(response.data);
+      console.log(alunos);
+    }
+
+    getData();
+  }, []);
 
   return (
     <Container>
+      <h1>Alunos Page</h1>
       <AlunosStyled>
-        <h1>Alunos Page</h1>
+        {alunos &&
+          alunos.map((aluno) => (
+            <div key={String(aluno.id)}>
+              <div className="profile-picture">
+                {get(aluno, 'Fotos[0].url', false) ? (
+                  <img src={aluno.Fotos[0].url} alt="foto do aluno" />
+                ) : (
+                  <FaUserCircle size={36} />
+                )}
+              </div>
+
+              <span>{aluno.nome} {aluno.sobrenome}</span>
+              <span>{aluno.email}</span>
+            </div>
+          ))}
       </AlunosStyled>
     </Container>
   );
--- src/pages/Alunos/styled.js
+++ src/pages/Alunos/styled.js
@@ -1,6 +1,25 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 const AlunosStyled = styled.section`
+  /* background-color: red; */
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+
+  div {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 5px 0;
+
+    .profile-picture {
+      img {
+        width: 36px;
+        height: 36px;
+        border-radius: 50%;
+      }
+    }
+  }
 `;
 
 export default AlunosStyled;
